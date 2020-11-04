4788d1962d25b758e76bcc1f3e4d547b14450707
anabcuelbas <abcuelbas@gmail.com>
2020-11-04 15:47:35 -0300

    Added multiple pipes spawn

---
+++ Assets/SpawnPipes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class SpawnPipes : MonoBehaviour
+{
+    public GameObject pipe;
+    public float height;
+    public float maxTime = 2f;
+
+    private float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newPipe = Instantiate(pipe);
+        newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime)
+        {
+            GameObject newPipe = Instantiate(pipe);
+            newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newPipe, 10f);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
